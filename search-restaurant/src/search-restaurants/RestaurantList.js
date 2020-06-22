307e9abb69a595a06cce15cef55cbda0051f6e51
Senthil Kumar Subburaj <senthil@Senthils-MBP.cgocable.net>
2020-06-22 14:19:34 -0400

    refactoring  the code

--- search-restaurant/src/search-restaurants/RestaurantList.js
+++ search-restaurant/src/search-restaurants/RestaurantList.js
@@ -4,21 +4,31 @@ import {
     refineSearch, clearFilters
 } from './actions'
 export default () => {
-    const { filteredRestaurants, refine, count} = useSelector(state => state.search, shallowEqual);
+    const { filteredRestaurants, refine, count } = useSelector(state => state.search, shallowEqual);
     const dispatch = useDispatch();
-    const renderRestuarants = filteredRestaurants.map((restaurant) => {
-        return (
-            <div className="card w-75 mb-3" key={restaurant.id}>
-                <div className="card-body">
-                    <h5 className="card-title"><strong>{restaurant.name}</strong></h5>
-                    <div className="card-sub-title">{restaurant.address}, {restaurant.city}, {restaurant.state},{restaurant.postal_code}</div>
-                    <div className="card-text">Price: {restaurant.price}</div>
-                    <a href={restaurant.reserve_url} className="card-link">Reserve</a>
+
+    const renderRestuarants = () => {
+        if (filteredRestaurants && filteredRestaurants.length === 0) {
+            return (
+                <p className="text-danger">
+                    NO RECORDS TO DISPLAY
+                     </p>
+            )
+        }
+        return filteredRestaurants.map((restaurant) => {
+            return (
+                <div className="card w-75 mb-3" key={restaurant.id}>
+                    <div className="card-body">
+                        <h5 className="card-title"><strong>{restaurant.name}</strong></h5>
+                        <div className="card-sub-title">{restaurant.address}, {restaurant.city}, {restaurant.state},{restaurant.postal_code}</div>
+                        <div className="card-text">Price: {`$${parseFloat(restaurant.price).toFixed(2)}`}</div>
+                        <a href={restaurant.reserve_url} className="card-link">Reserve</a>
+                    </div>
                 </div>
-            </div>
-        )
+            )
+        })
     }
-    );
+
     const handleRefine = (refine) => {
         (refine.length > 0) ? dispatch(refineSearch(refine)) : dispatch(clearFilters())
     }
@@ -40,16 +50,8 @@ export default () => {
 
                 </div> : null
             }
-            {
-                (count === 0 || filteredRestaurants.length === 0) ?
-                   <p 
-                        className="text-danger">
-                           NO RECORDS TO DISPLAY
-                    </p> 
-                : null
-            }
             <div className="d-flex flex-row flex-wrap justify-content-between">
-                {renderRestuarants}
+                {renderRestuarants()}
             </div>
         </div>
     )
